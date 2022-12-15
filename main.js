6e6aed6b93706790e3b2739a0dba138f4232a059
mugilavathi <mugirajan95@gmail.com>
2022-12-15 10:38:33 +0530

    add my file

---
+++ main.js
@@ -0,0 +1,9 @@
+var i,j 
+var n=5;
+var alpha=["A","B","C","D","E"]
+    for (i=0;i<=n;i++){
+        for(j=0;j<i;j++){
+            document.write(alpha[j]);
+        }
+        document.write("<br>");
+    }
